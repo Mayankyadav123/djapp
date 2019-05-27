81d9b63ac907461a03c3b7e9a253bd0530ac4e80
MOHIT <mohit.kumar@external.merckgroup.com>
2019-05-27 19:03:35 +0530

    cleanup

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,54 +1,60 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getCurrentProfile } from "../../actions/profileActions";
-import Spinner from "../common/spinner.gif";
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/spinner.gif';
+import { bindActionCreators } from 'redux';
 class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getCurrentProfile();
-  }
-  render() {
-    const { user } = this.props.auth;
-    console.log(this.props);
-    // const { profile, loading } = this.props.profile;
+	componentDidMount() {
+		this.props.getCurrentProfile();
+	}
+	render() {
+		const { user } = this.props.auth;
+		console.log(this.props);
+		const { profile, loading } = this.props;
 
-    let dashboardContent;
-    // console.log(profile + ":" + loading);
-    // if (profile === null || loading) {
-    //   dashboardContent = <Spinner />;
-    // } else {
-    //   dashboardContent = <h1>Hello</h1>;
-    // }
-    return (
-      <div>
-        <div className="dashboard">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <h1 className="display-4">Dashboard</h1>
-                {dashboardContent}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		let dashboardContent;
+		console.log(profile + ':' + loading);
+
+		if (profile && !loading) {
+			dashboardContent = <h1>Hello</h1>;
+		} else {
+			dashboardContent = <h1>Loading</h1>;
+		}
+
+		return (
+			<div>
+				<div className='dashboard'>
+					<div className='container'>
+						<div className='row'>
+							<div className='col-md-12'>
+								<h1 className='display-4'>Dashboard</h1>
+								{dashboardContent}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-  profile: state.profile,
-  auth: state.auth
+	profile: state.profile,
+	auth: state.auth,
 });
 
+const mapDisProp = dispatch => {
+	return bindActionCreators({ getCurrentProfile }, dispatch);
+};
+
 export default connect(
-  mapStateToProps,
-  { getCurrentProfile }
+	mapStateToProps,
+	mapDisProp,
 )(Dashboard);
